de11f601413ceeb1989de0fb6e11c2daa6bca69b
Dinh Nguyen Anh Dung <dungdna2000@gmail.com>
2025-09-11 14:00:03 +0700

    warmup execercise

--- main.cpp
+++ main.cpp
@@ -2,7 +2,31 @@
 
 using namespace std;
 
+void week01() {
+
+#define MAX 10
+	
+	float x[MAX];
+	float y[MAX];
+
+	int n; 
+	cout << "Nhap so diem: ";
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cout << "x[" << i << "]: "; cin >> x[i];
+		cout << "y[" << i << "]: "; cin >> y[i];
+	}
+
+	float s = 0;
+	for (int i = 0; i < n; i++) {
+		s += sqrt(x[i] * x[i] + y[i] * y[i]);
+	}
+
+	cout << "Tong khoang cach: " << s << endl;
+}
+
 int main(int) {
-	cout << "Hello, world!" << endl;
+	week01();
 	return 1;
 }
